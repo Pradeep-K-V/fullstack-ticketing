1d841b83ac87838599f21d8ac65ae84f510c852d
Pradeep K V <Pradeephegde113@gmail.com>
2025-12-03 00:35:23 +0530

    changed index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const { jwtAuth } = require('./middleware/jwtAuth');
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://fullstack-ticketing.vercel.app',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());

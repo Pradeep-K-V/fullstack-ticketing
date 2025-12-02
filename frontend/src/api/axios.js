c862e8b8e2d7538aba6d14748e5e7bfff82bbdfe
Pradeep K V <Pradeephegde113@gmail.com>
2025-12-02 22:05:28 +0530

    changed axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_BASE || 'https://fullstack-ticketing.onrender.com/api'
 });
 
 // attach JWT from localStorage
